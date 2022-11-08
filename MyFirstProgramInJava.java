e458b3e84c8d83aed005a87209971af63a8404ce
RaidoxxMPH <suporterdx@gmail.com>
2022-11-08 15:35:07 -0300

    My first program in Java

---
+++ MyFirstProgramInJava.java
@@ -0,0 +1,11 @@
+class MyFirstProgramInJava
+{
+  public static void main(String dados[])
+  {
+    int idade = 18;
+    String nome = "Raidoxx";
+    
+    System.out.println("Hello, my name is "+ nome +", I'm "+ idade +" years old");
+    System.exit(0);
+  }
+}
